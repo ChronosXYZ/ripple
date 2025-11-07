e2b66eb72d70b61ee2e8cbbaf9166e2c9d8cbd78
ChronosXYZ <chronosxyz000@gmail.com>
2025-11-07 21:10:19 +0300

    Remove tauri GUI
    Unfortunately, Tauri doesn't work well on Linux due to webkit2gtk

--- tauri-gui/src/pages/Messages.tsx
+++
@@ -1,7 +0,0 @@
-export function MessagesPage() {
-    return (
-        <div className="flex items-center justify-center min-h-full">
-            <span className="font-mono">This is the Messages page content.</span>
-        </div>
-    );
-}
--- tauri-gui/src/pages/NetworkStatus.tsx
+++
@@ -1,7 +0,0 @@
-export function NetworkStatusPage() {
-    return (
-        <div className="flex items-center justify-center min-h-full">
-            <span className="font-mono">This is the Network Status page content.</span>
-        </div>
-    );
-}
